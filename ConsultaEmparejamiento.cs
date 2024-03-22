9efa121056acb8389be8503a6df537308c5bf32b
Alexis Alexander Bolio Chan <23091192@alumno.utmetropolitana.edu.mx>
2024-03-22 12:54:13 -0600

    Hola

--- ConsultaEmparejamiento.cs
+++ ConsultaEmparejamiento.cs
@@ -1,7 +1,14 @@
+namespace GBDS2;
 class ConsultaEmparejamiento
 {
+    ConexionBD conexionBD = new ConexionBD();
     public void Usuario()
     {
-        
+      string query =""
+
+      using (SqlCommand command = new SqlCommand(query,conexionBD.AbrirConexion()))
+      {
+
+      }
     }
 }
